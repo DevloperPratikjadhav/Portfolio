1228d001a98cca8cb4c195e3ba2545efbffe7cde
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-07-03 17:31:01 +0530

    portfolio completed

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+var typed = new Typed("#typing-text", {
+    strings: ["Empty","Web designing","Frontend development","Web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+});
